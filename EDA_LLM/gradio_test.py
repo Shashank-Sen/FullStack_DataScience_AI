f7d459c2e7a6b774f3edb46343925611f38dd059
Shashank Sen <sha2nk02@gmail.com>
2025-11-04 23:04:50 +0530

    Add files via upload

---
+++ EDA_LLM/gradio_test.py
@@ -0,0 +1,14 @@
+import gradio as gr
+
+def greet(name, intensity):
+    return "Hello, " + name + "!" * int(intensity)
+
+demo = gr.Interface(
+    fn=greet,
+    inputs=["text", "slider"],
+    outputs=["text"],
+)
+
+demo.launch()
+
+# To run the code type "python File_name.py" in the terminal 
